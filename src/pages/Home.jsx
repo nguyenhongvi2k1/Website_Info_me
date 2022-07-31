8315a3c44d7d0e3398cf4031dc3ebe0a452f34b6
nguyenhongvi2k1 <nguyenhongvi1906@gmail.com>
2022-07-31 13:23:53 +0700

    edit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,6 @@ import { Component } from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import "./style_pages.css"
-import img from '../assets/img/_MG_1474.jpeg';
 class Home extends Component {
     constructor(props) {
         super(props);
